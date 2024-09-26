39ba0b3e8880f44b70f85abf31e35a742b8e7c5a
Shay Rozen <11279672+shyRozen@users.noreply.github.com>
2024-09-26 20:34:44 +0300

    Update main.tf with uncomment the encoding

--- upi/vsphere/vm/main.tf
+++ upi/vsphere/vm/main.tf
@@ -53,7 +53,7 @@ resource "vsphere_virtual_machine" "vm" {
   
   extra_config = {
     "guestinfo.ignition.config.data"           = data.external.gzip_base64.result.encoded
-   # "guestinfo.ignition.config.data.encoding"  = "gzip+base64"
+    "guestinfo.ignition.config.data.encoding"  = "gzip+base64"
     "guestinfo.afterburn.initrd.network-kargs" = "ip=[${var.ipaddress}]::[${cidrhost(var.machine_cidr, 1)}]:${local.cidr_prefix}:${var.vmname}:ens192:none:${join(":", [for dns in var.dns_addresses : format("[%s]", dns)])}"
     "stealclock.enable"                        = "TRUE"
   }
